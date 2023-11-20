ca12664288defb242b39057fe75376b87796b4c2
x86skwizer <yassineamrire@protonmail.com>
2023-11-20 01:45:00 -0800

    0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * sorted_list - checks if array is sorted
+ * @array: array
+ *
+ * Return: 0 if true
+ */
+int sorted_list(int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array[i] && i < size - 1)
+	{
+		if (array[i] > array[i + 1])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * bubble_sort - sorts in ascending order using Bubble sort algo
+ * @array: array of integers
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (array[i] > array[i + 1])
+		{
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			print_array(array, size);
+		}
+		i++;
+		if (i == size - 1 && sorted_list(array, size))
+			i = 0;
+	}
+}

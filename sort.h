0353438f5a26c048e0347caaf5f679efecbcf169
mberrouk <mberrouk@student.1337.ma>
2023-11-22 02:16:27 +0100

    update sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
-# define SORT_H
+#define SORT_H
 
-# include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,5 +21,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
